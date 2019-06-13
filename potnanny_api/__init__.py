53a1fd6ed962a7730493fbc70ddbddfd718ed15d
Jeffrey Leary <jeffreyleary@Jeffreys-MacBook-Air.local>
2019-06-12 21:12:18 -0400

    attempt at modifying jwt auth error handler to return proper http response instead of raising exceptions

--- potnanny_api/__init__.py
+++ potnanny_api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, current_app
+from flask import Flask, current_app, jsonify
 from .config import BaseConfig, Development, Production, Testing
 from .extensions import jwt
 from potnanny_core.database import init_engine, init_db, init_users
@@ -29,6 +29,30 @@ def config_extensions(app):
     jwt.init_app(app)
     jwt._set_error_handler_callbacks(app)
     # csrf.init_app(app)
+
+
+@jwt.expired_token_loader
+def my_expired_token_callback(expired_token):
+    token_type = expired_token['type']
+    return jsonify({
+        'status': 401,
+        'msg': 'The {} token has expired'.format(token_type)
+    }), 401
+
+
+@jwt.invalid_token_loader
+def my_invalid_token_callback(msg):
+    return jsonify({
+        'status': 401,
+        'msg': 'Invalid token: {}'.format(msg)
+    }), 401
+
+@jwt.unauthorized_loader
+def my_unauthorized_callback(msg):
+    return jsonify({
+        'status': 401,
+        'msg': 'Unauthorized: {}'.format(msg)
+    }), 401
 
 
 def config_api(app):
